b7f26ca4271664d4e41e95a94df6cd2388ee1b65
Errenris <138004039+Errenris@users.noreply.github.com>
2025-12-28 02:24:50 +0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,2 +1,18 @@
 import '../styles/globals.css'
-export default function App({ Component, pageProps }) { return <Component {...pageProps} /> }
+import Head from 'next/head'
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3215724068998032"
+          crossOrigin="anonymous"
+        ></script>
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  )
+}
